9086bf8679a3c1aa82e32ef766c12aeef86c6116
ZingerLittleBee <6970999@gmail.com>
2022-12-24 17:31:11 +0800

    refactor: 💡 runner

--- src-tauri/src/model/user.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Deserialize, Serialize, Default, Debug, Clone)]
-pub struct User {
-    pub uid: String,
-    pub gid: String,
-    pub name: String,
-    pub groups: Vec<String>,
-}
